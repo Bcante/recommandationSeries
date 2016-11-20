7b3e933a969b7b6bc153249cb2ee1819aff79ee1
benjamin <benjamin.denys7@etu.univ-lorraine.fr>
2016-11-20 16:27:57 +0100

    correction du homeCtrl

--- web/js/Controller/homeCtrl.js
+++ web/js/Controller/homeCtrl.js
@@ -1,4 +1,4 @@
 var app = angular.module('routeAppControllers');
-app.controller('home',['$scope','$location','$http','$rootScope','$window','$mdSidenav','$route','$interval',function ($scope,$location,$http,$rootScope,$window,$mdSidenav,$route,$interval) {
+app.controller('homeCtrl',['$scope','$location','$http','$rootScope','$window','$mdSidenav','$route','$interval',function ($scope,$location,$http,$rootScope,$window,$mdSidenav,$route,$interval) {
 
 }]);
