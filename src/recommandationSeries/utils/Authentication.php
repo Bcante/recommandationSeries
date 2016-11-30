6d99e8ac93ef1f3f599397ca9a425c7b14a4eebd
Benoît Cante <cantebenoit@hotmail.com>
2016-11-30 19:15:48 +0100

    Début de la gestion de l'inscription

--- src/recommandationSeries/utils/Authentication.php
+++ src/recommandationSeries/utils/Authentication.php
@@ -7,8 +7,58 @@ class Authentication {
 
     }
 
-    public static function register($name, $password, $email) {
+    public static function register($username, $password, $password_confirm, $email) {
+        // Check if the form is correctly filled
+        // Username has to be at least 4 chars long
+        // Password has to be at least 8 chars long, and contain a number
 
+        $inscOk = true;
+        // First level: Check if all inputs are set
+        if (!isset($username) || !isset($password) || !isset($password_confirm) || !isset($email)) {
+            echo "At least one of the field wasn't filled";
+            $inscOk = false;
+        }
+        // Second level: Filter and sanitize incoming inputs
+        else {
+            if (!filter_var($username, FILTER_VALDATE_STRING)) {
+                echo "Invalid username";
+                $inscOk = false;
+            }
+
+            if (!filter_var($password, FILTER_VALDATE_STRING)) {
+                echo "Invalid password";
+                $inscOk = false;
+            }
+
+            if (!filter_var($password_confirm, FILTER_VALDATE_STRING)) {
+                echo "Invalid password confirmation";
+                $inscOk = false;
+            }
+
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                echo "Invalid mail";
+                $inscOk = false;
+            }
+            // Third level: check if the inputs matches our policies
+            if ($inscOk) {
+                if(strlen($username) < 4) {
+                    echo "Username is not long enough";
+                    $inscOk = false;
+                }
+                if (!$password_confirm === $password) {
+                    echo "The two passwords didn't match";
+                    $inscOk = false;
+                }
+                if (!preg_match("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$", $password)) {
+                    echo "The password should be 8 letters long, contain a lowercase and a uppercase letter";
+                    $inscOk = false;
+                }
+            }
+        }
+        // Now that the inputs are sanitzed, we can check if they meet our policies
+
+
+        return $inscOk;
     }
 }
 ?>
