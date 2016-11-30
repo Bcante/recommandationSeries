9a36d4eb6563bd4394021266a9e838d2f18f7bfe
benjamin <benjamin.denys7@etu.univ-lorraine.fr>
2016-11-30 13:27:45 +0100

    "Ajout des services"

--- web/js/Controller/seriesCtrl.js
+++ web/js/Controller/seriesCtrl.js
@@ -3,7 +3,7 @@ var app = angular.module('routeAppControllers');
 app.controller('seriesCtrl',['$scope','$location','$http','$rootScope','$window','$mdSidenav','$route','$interval','serviceAjax',function ($scope,$location,$http,$rootScope,$window,$mdSidenav,$route,$interval,serviceAjax) {
 
 
-        serviceAjax.display.success(function (data, status, headers, config) {
+        serviceAjax.display(serieId).success(function (data, status, headers, config) {
             console.log(data);
             $scope.serieName = data.name;
         });
