53e94e7766dd52c680efb85ba7b01420a512f127
nicolas <nicolas.lamblin@outlook.com>
2016-12-04 11:38:17 +0100

    Amélioration connection

--- web/js/Controller/connexionCtrl.js
+++ web/js/Controller/connexionCtrl.js
@@ -1,17 +1,23 @@
 var app = angular.module('routeAppControllers');
 app.controller('connexionCtrl',['$scope','$location','$http','$rootScope','$window','$mdSidenav','$route','$interval',function ($scope,$location,$http,$rootScope,$window,$mdSidenav,$route,$interval) {
-	$scope.toConnect = function() {
+
+    $scope.connection = {
+        email : "",
+        password : ""
+    };
+
+    $scope.toConnect = function() {
         $http({
             method: 'POST',
             data: {
-                password: $scope.password,
-                email : $scope.email
+                password: $scope.connection.password,
+                email : $scope.connection.email
             },
             url: 'connexion'
         })
         .success(function (data, status, headers, config) {
             if(data == false) {
-                $scope.connexionError = "Invalid email / password"
+                $scope.connectionError = "Invalid email / password"
             }
             else {
                 localStorage.setItem('connected', true);
