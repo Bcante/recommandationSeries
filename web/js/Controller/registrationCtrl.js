dc2d7a288d02dab66fe3dba37a4630853c431b72
nicolas <nicolas.lamblin@outlook.com>
2016-12-04 11:32:52 +0100

    Résolution problème valeur dans confirm_password

--- web/js/Controller/registrationCtrl.js
+++ web/js/Controller/registrationCtrl.js
@@ -2,15 +2,25 @@ var app = angular.module('routeAppControllers');
 
 app.controller('registrationCtrl',['$scope','$location','$http','$rootScope','$window','$mdSidenav','$route','$interval',function ($scope,$location,$http,$rootScope,$window,$mdSidenav,$route,$interval) {
 
+    $scope.registration = {
+        username : "",
+        email : "",
+        password : "",
+        confirm_password : ""
+    };
+
     $scope.toRegistrate = function() {
-        // console.log($scope.registrationForm.confirm_password);
+        /*console.log($scope.registration.username);
+        console.log($scope.registration.password);
+        console.log($scope.registration.confirm_password);
+        console.log($scope.registration.email);*/
         $http({
             method: 'POST',
             data: {
-                username: username,
-                password: password,
-                password_confirm : confirm_password,
-                email : email
+                username: $scope.registration.username,
+                password: $scope.registration.password,
+                password_confirm : $scope.registration.confirm_password,
+                email : $scope.registration.email
             },
             url: 'registration'
         })
@@ -22,7 +32,7 @@ app.controller('registrationCtrl',['$scope','$location','$http','$rootScope','$w
 
 app.directive('equalsTo', [function () {
     /*
-     * <input ng-minlength="6" ng-model="password" name="password" type="password" required>
+     * <input ng-minlength="8" ng-pattern="/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/" ng-model="password" name="password" type="password" required>
      * <input ng-model="confirm_password" type="password" name="confirm_password" required equals-to="registrationForm.password">
      */
     return {
